d5ac2669aa27be2f1d4f5280886ccb309f15b9d0
Jayson Rael Ramos <lothlorien654@gmail.com>
2018-11-21 12:36:16 +0800

    Added Exam Migrations

--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text=models.CharField(max_length=120)
+
+    def __str__(self):
+       return self.question_text
+
+class Answer(models.Model):
+    answer_id=models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text=models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.answer_text
+
+class Choice(models.Model):
+    choice_id=models.ForeignKey(Answer, on_delete=models.CASCADE)
