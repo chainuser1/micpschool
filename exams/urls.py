b8246d8ed27360b7bbc7ceea53dfe72301404c4a
Jayson Rael Ramos <lothlorien654@gmail.com>
2018-11-23 11:20:34 +0800

    Addded User Authentication

--- exams/urls.py
+++ exams/urls.py
@@ -1,8 +1,9 @@
-__author__ = 'Pareng Je'
+__author__ = 'Pareng Jay'
 from django.urls import path
 
 from . import views
 
 urlpatterns=[
-    path('',views.index, name='index')
+    path('',views.index, name='home'),
+    path('auth/', views.auth, name='auth'),
 ]
--- exams/views.py
+++ exams/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
-def index(render):
-    return HttpResponse("You are at the exams part.")
+def index(request):
+    return render(request, 'exams/index.html')
+
+def auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if(user is not None):
+        login(request, user)
+        return redirect('exams/index.html')
+    else:
+        return redirect('exams/index.html')
