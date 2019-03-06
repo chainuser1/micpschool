11952f1f5b2afa4dc3a5d4c718c7b84e2ff2fd8b
Jayson Ramos <lothlorien654@gmail.com>
2019-03-06 11:19:53 +0800

    Index Carousel View Done

--- micpschool/settings.py
+++ micpschool/settings.py
@@ -176,8 +176,8 @@ USE_L10N = True
 USE_TZ = True
 
 #media files
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
