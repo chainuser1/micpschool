e79d99fdb3b0b71c23cf038bae198da52cf5b89d
Jayson Ramos <lothlorien654@gmail.com>
2019-03-07 11:24:04 +0800

    Made changes

--- news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='news_index'),
-    path('<int:news_id>/', views.details,name="details" ),
-    path('<int:news_id>/', views.comment_add, name="add")
-]
